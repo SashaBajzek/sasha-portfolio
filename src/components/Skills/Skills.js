f6666cbe6e44b354b2c048eb0eb8d96cbf276891
Sasha Bajzek <sasha.bajzek@gmail.com>
2020-05-10 23:26:18 -0700

    DRY up skills section

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,144 +1,107 @@
-import React, {Component} from 'react'
+import React from 'react'
+import {
+  reactLogo,
+  reduxLogo,
+  tsLogo,
+  htmlLogo,
+  sassLogo,
+  storybookLogo,
+  jestLogo,
+  cypressLogo,
+  sewing,
+  garden,
+  violin,
+  modelBridge,
+  bridge,
+  inspect,
+  foosball,
+  target,
+  brush,
+  camping,
+} from './icons/index.js'
 import './Skills.scss'
-import reactLogo from './icons/react.svg'
-import reduxLogo from './icons/redux.svg'
-import tsLogo from './icons/typescript.svg'
-import htmlLogo from './icons/HTML5_Badge.svg'
-import sassLogo from './icons/sass.svg'
-import sewing from './icons/sewing.svg'
-import garden from './icons/garden.svg'
-import violin from './icons/violin.svg'
-import modelBridge from './icons/Bridge--Small.jpg'
-import bridge from './icons/bridge.svg'
-import inspect from './icons/magnifying_glass.svg'
-import foosball from './icons/foosball.svg'
-import target from './icons/target.svg'
-import makeup from './icons/makeup.svg'
-import camping from './icons/camping.svg'
-// painting, accessiblity, jest, cypress
 
-class Skills extends Component {
-  render() {
-    return (
-      <section id="scrollTo--skills" className="section--skills">
-        <div className="container--skills">
-          <h2 className="section__header">My Skills</h2>
-          <ul className="list--skills-main">
-            <li>
-              <h3 className="list__heading--skills-main">Software</h3>
-              <ul className="list--skills-sub">
-                <li>
-                  <a className="skill" href="https://facebook.github.io/react/">
-                    <p className="skills__name">React</p>
-                    <img className="skills__logo" src={reactLogo} alt="React Logo" />
+const Skills = () => {
+  const softwareSkills = [
+    {image: reactLogo, link: 'https://facebook.github.io/react/', text: 'React'},
+    {image: reduxLogo, link: 'http://redux.js.org/', text: 'Redux'},
+    {image: tsLogo, link: 'https://www.typescriptlang.org/', text: 'TypeScript'},
+    {image: htmlLogo, link: 'https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5', text: 'HTML5'},
+    {image: sassLogo, link: 'http://sass-lang.com/', text: 'Sass'},
+    {image: storybookLogo, link: 'https://storybook.js.org/', text: 'Storybook'},
+    {image: jestLogo, link: 'https://jestjs.io/', text: 'Jest'},
+    {image: cypressLogo, link: 'https://www.cypress.io/', text: 'Cypress'},
+  ]
+  const hardwareSkills = [
+    {image: brush, link: 'https://www.bobross.com/', text: 'Oil Painting'},
+    {image: garden, link: 'https://en.wikipedia.org/wiki/Gardening', text: 'Gardening'},
+    {image: violin, link: 'https://www.britannica.com/art/violin', text: 'Violin'},
+    {image: sewing, link: 'https://en.wikipedia.org/wiki/Sewing#Garment_construction', text: 'Sewing'},
+    {
+      image: modelBridge,
+      link:
+        'https://web.iit.edu/mediaroom/press-releases/2005/apr/25/tinley-park-high-school-student-wins-28th-international-high',
+      text: 'Model Bridge Building',
+    },
+  ]
+  const practicalSkills = [
+    {image: camping, link: 'https://www.w3.org/WAI/fundamentals/accessibility-intro/', text: 'Accessibility'},
+    {image: bridge, link: 'https://en.wikipedia.org/wiki/Structural_engineering', text: 'Structural Engineering'},
+    {image: inspect, link: 'https://www.fhwa.dot.gov/bridge/inspection/', text: 'Bridge Inspecting'},
+    {image: foosball, link: 'https://en.wikipedia.org/wiki/Table_football', text: 'Foosball'},
+    {image: target, link: 'https://firearmtraining.nra.org/become-an-instructor/', text: 'Pistol Instructor'},
+    {image: camping, link: 'https://en.wikipedia.org/wiki/Camping', text: 'Camping'},
+  ]
+  return (
+    <section id="scrollTo--skills" className="section--skills">
+      <div className="container--skills">
+        <h2 className="section__header">My Skills</h2>
+        <ul className="list--skills-main">
+          <li>
+            <h3 className="list__heading--skills-main">Software</h3>
+            <ul className="list--skills-sub">
+              {softwareSkills.map(item => (
+                <li key={item.text}>
+                  <a className="skill" href={item.link}>
+                    <p className="skills__name">{item.text}</p>
+                    <img className="skills__logo" src={item.image} alt="" />
                   </a>
                 </li>
-                <li>
-                  <a className="skill" href="http://redux.js.org/">
-                    <p className="skills__name">Redux</p>
-                    <img className="skills__logo" src={reduxLogo} alt="Redux Logo" />
-                  </a>
-                </li>
-                <li>
-                  <a className="skill" href="https://www.typescriptlang.org/">
-                    <p className="skills__name">TypeScript</p>
-                    <img className="skills__logo" src={tsLogo} alt="TypeScript Logo" />
-                  </a>
-                </li>
-                <li>
-                  <a className="skill" href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5">
-                    <p className="skills__name">HTML5</p>
-                    <img className="skills__logo" src={htmlLogo} alt="HTML5 Logo" />
-                  </a>
-                </li>
-                <li>
-                  <a className="skill" href="http://sass-lang.com/">
-                    <p className="skills__name">Sass</p>
-                    <img className="skills__logo" src={sassLogo} alt="Sass Logo" />
-                  </a>
-                </li>
-              </ul>
-            </li>
+              ))}
+            </ul>
+          </li>
 
-            <li>
-              <h3 className="list__heading--skills-main">Hardware</h3>
-              <ul className="list--skills-sub">
-                <li>
-                  <a className="skill" href="https://en.wikipedia.org/wiki/Sewing#Garment_construction">
-                    <p className="skills__name">Sewing</p>
-                    <img className="skills__logo" src={sewing} alt="Sewing Thread" />
+          <li>
+            <h3 className="list__heading--skills-main">Hardware</h3>
+            <ul className="list--skills-sub">
+              {hardwareSkills.map(item => (
+                <li key={item.text}>
+                  <a className="skill" href={item.link}>
+                    <p className="skills__name">{item.text}</p>
+                    <img className="skills__logo" src={item.image} alt="" />
                   </a>
                 </li>
-                <li>
-                  <a className="skill" href="https://en.wikipedia.org/wiki/Gardening">
-                    <p className="skills__name">Gardening</p>
-                    <img className="skills__logo" src={garden} alt="Gardening Gloves" />
-                  </a>
-                </li>
-                <li>
-                  <a className="skill" href="https://www.britannica.com/art/violin">
-                    <p className="skills__name">Violin</p>
-                    <img className="skills__logo" src={violin} alt="Violin" />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="skill"
-                    href="https://web.iit.edu/mediaroom/press-releases/2005/apr/25/tinley-park-high-school-student-wins-28th-international-high"
-                  >
-                    <p className="skills__name">Model Bridge Building</p>
-                    <img className="skills__logo" src={modelBridge} alt="Bridge" />
-                  </a>
-                </li>
-              </ul>
-            </li>
+              ))}
+            </ul>
+          </li>
 
-            <li>
-              <h3 className="list__heading--skills-main">Practical</h3>
-              <ul className="list--skills-sub">
-                <li>
-                  <a className="skill" href="https://en.wikipedia.org/wiki/Structural_engineering">
-                    <p className="skills__name">Structural Engineering</p>
-                    <img className="skills__logo" src={bridge} alt="Bridge" />
+          <li>
+            <h3 className="list__heading--skills-main">Practical</h3>
+            <ul className="list--skills-sub">
+              {practicalSkills.map(item => (
+                <li key={item.text}>
+                  <a className="skill" href={item.link}>
+                    <p className="skills__name">{item.text}</p>
+                    <img className="skills__logo" src={item.image} alt="" />
                   </a>
                 </li>
-                <li>
-                  <a className="skill" href="https://www.fhwa.dot.gov/bridge/inspection/">
-                    <p className="skills__name">Bridge Inspecting</p>
-                    <img className="skills__logo" src={inspect} alt="Magnifying Glass" />
-                  </a>
-                </li>
-                <li>
-                  <a className="skill" href="https://en.wikipedia.org/wiki/Table_football">
-                    <p className="skills__name">Foosball</p>
-                    <img className="skills__logo" src={foosball} alt="Foosball Table" />
-                  </a>
-                </li>
-                <li>
-                  <a className="skill" href="https://firearmtraining.nra.org/become-an-instructor/">
-                    <p className="skills__name">Pistol Instructor</p>
-                    <img className="skills__logo" src={target} alt="Target" />
-                  </a>
-                </li>
-                <li>
-                  <a className="skill" href="https://en.wikipedia.org/wiki/Make-up_artist">
-                    <p className="skills__name">Amatuer Makeup Artist</p>
-                    <img className="skills__logo" src={makeup} alt="Makeup Brush" />
-                  </a>
-                </li>
-                <li>
-                  <a className="skill" href="https://en.wikipedia.org/wiki/Camping">
-                    <p className="skills__name">Camping</p>
-                    <img className="skills__logo" src={camping} alt="Tent" />
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </section>
-    )
-  }
+              ))}
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
 }
 
 export default Skills
---
+++ src/components/Skills/icons/index.js
@@ -0,0 +1,39 @@
+import reactLogo from './react.svg'
+import reduxLogo from './redux.svg'
+import tsLogo from './typescript.svg'
+import htmlLogo from './HTML5_Badge.svg'
+import sassLogo from './sass.svg'
+import storybookLogo from './storybook.svg'
+import jestLogo from './jest.svg'
+import cypressLogo from './cypress.svg'
+import sewing from './sewing.svg'
+import garden from './garden.svg'
+import violin from './violin.svg'
+import modelBridge from './Bridge--Small.jpg'
+import bridge from './bridge.svg'
+import inspect from './magnifying_glass.svg'
+import foosball from './foosball.svg'
+import target from './target.svg'
+import brush from './brush.svg'
+import camping from './camping.svg'
+
+export {
+  reactLogo,
+  reduxLogo,
+  tsLogo,
+  htmlLogo,
+  sassLogo,
+  storybookLogo,
+  jestLogo,
+  cypressLogo,
+  sewing,
+  garden,
+  violin,
+  modelBridge,
+  bridge,
+  inspect,
+  foosball,
+  target,
+  brush,
+  camping,
+}
