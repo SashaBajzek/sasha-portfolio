fe8df9e101489d3f5b061011426f0272047e77d6
Sasha Bajzek <sasha.bajzek@gmail.com>
2020-05-10 23:44:49 -0700

    Reorganize Welcome

---
+++ src/components/Welcome/svgs/index.js
@@ -0,0 +1,8 @@
+import Alien from './Alien/Alien'
+import Sunflower from './Sunflower/Sunflower'
+import SunflowerPetals from './SunflowerPetals/SunflowerPetals'
+import Tulips from './Tulips/Tulips'
+import Vine1 from './Vine1/Vine1'
+import Vine2 from './Vine2/Vine2'
+
+export {Alien, Sunflower, SunflowerPetals, Tulips, Vine1, Vine2}
